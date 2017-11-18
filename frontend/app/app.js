29c3210f044ea7ef26506d46b7c852969c88accb
DESKTOP-GO8OUUP\12276 <mytkeroro@sjtu.edu.cn>
2017-11-18 14:10:15 +0800

    第二次提交

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,11 +1,26 @@
-(function(){
+(function () {
     'use strict';
 
-    angular.module('NameCalculator',[])
-        .controller('NameCalculatorController',function($scope){
-            $scope.name = "";
-            $scope.totalValue =0;
-        });
+        angular.module('DIApp', [])
+    .controller('DIController', DIController);
 
+        function DIController ($scope,
+                                $filter,
+                                $injector) {
+          $scope.name = "hahahahahha";
 
-})();
+              $scope.upper = function () {
+                var upCase = $filter('uppercase');
+                $scope.name = upCase($scope.name);
+              };
+
+              console.log($injector.annotate(DIController));
+        }
+
+        function AnnonateMe(name, job, blah) {
+              return "asdasdasdasdsa";
+            }
+
+    console.log(DIController.toString());
+
+        })();
